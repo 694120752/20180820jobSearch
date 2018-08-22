7c40c790c46e7fa4722b9dfbdf6bd3af52e9afe3
Zjs <694120752@qq.com>
2018-08-22 17:53:02 +0800

    初始化工程
    啥也没有 空的工程文件

---
+++ TodayHotRecruit/TodayHotRecruit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TodayHotRecruit
+//
+//  Created by sn_zjs on 2018/8/22.
+//  Copyright © 2018年 sn_zjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TodayHotRecruit/TodayHotRecruit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TodayHotRecruit
+//
+//  Created by sn_zjs on 2018/8/22.
+//  Copyright © 2018年 sn_zjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
